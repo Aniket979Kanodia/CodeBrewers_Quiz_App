7789b1d957f1ef1dc7f2bc05f2b5e13eee6dbcef
Aniket979Kanodia <kanodiaaniket979@gmail.com>
2022-07-16 02:39:57 +0530

    Added database and created function to add test

---
+++ server/db/connection.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb://localhost:27017/QuizzApp",{ useNewUrlParser: true, useUnifiedTopology: true  })
+.then(()=>console.log("connection successful....."))
+.catch((err)=>console.log(err));
+
+const adminSchema = new mongoose.Schema({
+   name: String,
+   email: String,
+   pass: String,
+   quizzDetails: [{
+    testId: Number,
+    status: Boolean
+
+   }]
+})
+
+const userSchema = new mongoose.Schema({
+   name: String,
+   email: String,
+
+   quizzes: [{
+    testId: Number,
+    marks: Number,
+   }]
+})
+
+const testSchema = new mongoose.Schema({
+    // testId: Number,
+    // startTime: Date,
+    // endTime: Date,
+   // duration: Number,
+    //response_code: Number,
+   // adminId: Number,
+   // users: Number,
+    questions: [{
+        options: Array,
+        question: String,
+        correct_answer: String,
+        incorrect_answers: Array
+    }]
+})
+
+function insertTest(duration,questions){
+    newTest= new testSchema({
+        
+    
+   // duration: duration,
+    questions: questions
+    })
+    newTest.save();
+}
+
+
+
+
